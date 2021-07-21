f606b79f6f4498044760cd85a36725be8051f670
LEEJUNJAE <aquarius4785@gmail.com>
2021-07-22 01:27:10 +0900

    dll add

--- Engine/Gui.h
+++ Engine/Gui.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_sdl.h"
+#include "ImGui/imgui_impl_opengl3.h"
+
+
 class Gui
 {
+public:
+	Gui() {}
+	~Gui() {}
+
+	static InitGui();
+
+private:
+	ImGuiIO* io;
 };
 
