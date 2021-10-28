d5ede2462f57bd1837efc5a9576251ba183f1b11
aquarius4785@gmail.com <aquarius4785@gmail.com>
2021-10-28 13:04:25 +0900

    Version1.00

--- Engine/Collision.cpp
+++ Engine/Collision.cpp
@@ -1,18 +1,3 @@
-// ----------------------------------------------------------------
-// From Game Programming in C++ by Sanjay Madhav
-// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
-// 
-// Released under the BSD License
-// See LICENSE in root directory for full details.
-// ----------------------------------------------------------------
-// MinDistSq between two line segments:
-// Copyright 2001 softSurfer, 2012 Dan Sunday
-// This code may be freely used, distributed and modified for any purpose
-// providing that this copyright notice is included with it.
-// SoftSurfer makes no warranty for this code, and cannot be held
-// liable for any real or imagined damage resulting from its use.
-// Users of this code must verify correctness for their application.
-// ----------------------------------------------------------------
 
 #include "Collision.h"
 #include <algorithm>
--- Engine/VertexArray.cpp
+++ Engine/VertexArray.cpp
@@ -1,10 +1,4 @@
-// ----------------------------------------------------------------
-// From Game Programming in C++ by Sanjay Madhav
-// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
-// 
-// Released under the BSD License
-// See LICENSE in root directory for full details.
-// ----------------------------------------------------------------
+
 
 #include "VertexArray.h"
 #include <GL/glew.h>
